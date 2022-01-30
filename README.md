38b735deb2a5d6ae0e035cec79ab961a46bc92ec
Aaron Holmes <aaron@aaronholmes.net>
2022-01-29 16:16:31 -0800

    Update with the latest from codepen w/ the ability to superimpose waves.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Waves
 
-This is an interactive visual art project.
+Draw sine waves with the use of draggable sliders.
 
-Draws sine waves that are customizable through sliders.
+A primary wave is drawn as the summation of each line added to the page.
+
+To use, click "CREATE WAVE," then drag the sliders to change the wave shape.
+
+The project can be found on [codepen.io](https://codepen.io/aholmes/full/gBNbKN)!
+
+This project was inspired by the "wave puzzle" in Myst IV: Revelation.
+
+![](img/606322-920187_20040924_004.jpg)
+
+(Image credit [gamespot.com](https://www.gamespot.com/articles/myst-iv-revelation-final-impressions/1100-6108695/)).
--- js/main.js
+++ js/main.js
@@ -1,171 +1,199 @@
-'use strict';
+"use strict";
 
-function getRandRgb()
-{
+function getRandRgb() {
     return Math.floor(Math.random() * 0x1000000);
 }
 
-function hexToRgbString(hex)
-{
-    var rgb = {
-        r: hex >> 16 & 0xff,
-        g: hex >> 8 & 0xff,
+function hexToRgbString(hex) {
+    let rgb = {
+        r: (hex >> 16) & 0xff,
+        g: (hex >> 8) & 0xff,
         b: hex & 0xff
     };
 
-    return 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+    return "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
 }
 
-var controls = document.getElementById('controls');
-function createWave()
-{
-    var inputs = {
-        amplitude: document.createElement('input'),
-        period: document.createElement('input'),
-        phase: document.createElement('input')
-    }
+let controls = document.getElementById("controls");
+function createWave() {
+    let inputs = {
+        amplitude: document.createElement("input"),
+        period: document.createElement("input"),
+        phase: document.createElement("input")
+    };
 
-    inputs.amplitude.type = 'range';
+    inputs.amplitude.type = "range";
     inputs.amplitude.step = 0.01;
     inputs.amplitude.min = -1;
     inputs.amplitude.max = 1;
     inputs.amplitude.value = 0;
 
-    inputs.period.type = 'range';
+    inputs.period.type = "range";
     inputs.period.step = 0.01;
-    inputs.period.min = 0;
+    inputs.period.min = 0.01;
     inputs.period.max = 100;
     inputs.period.value = 1;
 
-    inputs.phase.type = 'range';
+    inputs.phase.type = "range";
     inputs.phase.step = 0.01;
     inputs.phase.min = -360;
     inputs.phase.max = 360;
     inputs.phase.value = 0;
 
-    var waveColor = getRandRgb();
+    let waveColor = getRandRgb();
     window.waves.push({
-        options:{color:waveColor},
-        wave:{
-            amplitude:inputs.amplitude.value,
-            period:inputs.period.value,
-            phase:inputs.phase.value
+        options: { color: waveColor },
+        wave: {
+            amplitude: inputs.amplitude.value,
+            period: inputs.period.value,
+            phase: inputs.phase.value
         }
     });
-    var waveCount = window.waves.length - 1;
+    let waveCount = window.waves.length - 1;
 
-    inputs.amplitude.addEventListener('input', function()
-    {
-        waves[waveCount].wave.amplitude = this.value * (pixiApp.renderer.height / 2);
-        broadcastWaveChange(this, waves[waveCount].wave);
+    inputs.amplitude.addEventListener("input", function () {
+        waves[waveCount].wave.amplitude =
+            this.value * (pixiApp.renderer.height / 2);
     });
-    inputs.period.addEventListener('input', function()
-    {
+    inputs.period.addEventListener("input", function () {
         waves[waveCount].wave.period = +this.value;
-        broadcastWaveChange(this, waves[waveCount].wave);
     });
-    inputs.phase.addEventListener('input', function()
-    {
+    inputs.phase.addEventListener("input", function () {
         waves[waveCount].wave.phase = toRad(this.value);
-        broadcastWaveChange(this, waves[waveCount].wave);
     });
 
-    var controlContainer = document.createElement('div');
-    controlContainer.classList.add('wave');
+    let controlContainer = document.createElement("div");
+    controlContainer.classList.add("wave");
     controlContainer.style.backgroundColor = hexToRgbString(waveColor);
-    for(var input in inputs)
-    {
-        if (!inputs.hasOwnProperty(input)) continue;
-        controlContainer.appendChild(inputs[input]);
+    let btn = document.createElement("button");
+    btn.classList.add("wave-close");
+    btn.onclick = () => {
+        let wave = waves[waveCount];
+        delete waves[waveCount];
+        wave.line.clear();
+        controls.removeChild(controlContainer);
+        window.dispatchEvent(new Event("resize"));
+    };
+    controlContainer.appendChild(btn);
+    let inputContainer = document.createElement("div");
+    controlContainer.appendChild(inputContainer);
+    for (let input of Object.entries(inputs)) {
+        //if (!inputs.hasOwnProperty(input)) continue;
+        let p = document.createElement('p');
+        let span = document.createElement('span');
+        span.innerText = input[0];
+        p.appendChild(span);
+        p.appendChild(input[1]);
+        inputContainer.appendChild(p);
     }
     controls.appendChild(controlContainer);
+    window.dispatchEvent(new Event("resize"));
 }
 
-function broadcastWaveChange(sender, wave)
-{
-    var event = new CustomEvent('waveChange', {detail:{sender:sender, wave:wave}});
-    document.dispatchEvent(event);
+function toRad(angle) {
+    return (angle * Math.PI) / 180;
 }
 
-function toRad(angle)
-{
-    return angle * Math.PI / 180;
+function getYFromAngleForWave(wave, angle) {
+    if (!wave) throw new Error('Wave is empty!');
+
+    let c = (360 / pixiApp.renderer.width) * wave.period;
+
+    let y1 = Math.sin((toRad(angle) - wave.phase / c) * c) * wave.amplitude;
+    let y2 = Math.sin((toRad(angle + 1) - wave.phase / c) * c) * wave.amplitude;
+    
+    return [y1, y2];
 }
 
-function draw(delta, pixiApp)
-{
-    for(var waveId in waves)
+function drawLineSegmentFromAngle(y1, y2, angle, line, pixiApp) {
+    line.moveTo(angle, pixiApp.renderer.height / 2 - y1);
+    line.lineTo(angle + 1, pixiApp.renderer.height / 2 - y2);
+}
+
+function configureLineComponentForWave(wave) {
+    if (wave.line) {
+        wave.line.clear();
+    } else {
+        wave.line = new PIXI.Graphics();
+    }
+    
+    wave.line.lineStyle(
+        wave.options.width ?? 1,
+        wave.options.color,
+        1);
+}
+
+function draw(delta, pixiApp) {
+    // draw wave 0
+    configureLineComponentForWave(waves[0]);
+    for (let angle = 0; angle < pixiApp.renderer.width - 1; angle++) {
+        let y1 = 0;
+        let y2 = 0;
+        // wave 0 is additive of all other lines, so we take
+        // the amplitude, period, and phase of each line and add them
+        for (let wave of waves.slice(1)){
+            if (!wave) continue;
+            let y = getYFromAngleForWave(wave.wave, angle);
+            y1 += y[0];
+            y2 += y[1];
+        }
+        
+        drawLineSegmentFromAngle(y1, y2, angle, waves[0].line, pixiApp);
+    }
+    pixiApp.stage.addChild(waves[0].line);
+    
+    // draw the other waves
+    for (let wave of waves.slice(1)) {
+        if (!wave) continue;
+        
+        configureLineComponentForWave(wave);
+
+        for (let angle = 0; angle < pixiApp.renderer.width - 1; angle++) {
+            let [y1, y2] = getYFromAngleForWave(wave.wave, angle);
+            drawLineSegmentFromAngle(y1, y2, angle, wave.line, pixiApp);
+        }
+        
+        pixiApp.stage.addChild(wave.line);
+    }
+}
+
+function getWaveDisplayHeight() {
+    return window.innerHeight - controls.clientHeight;
+}
+
+function resize(renderer) {
+    renderer.resize(document.body.clientWidth, getWaveDisplayHeight());
+}
+
+window.waves = [
     {
-        var waveOptions = waves[waveId];
-        var options = waveOptions.options;
-        var wave = waveOptions.wave;
-        var line = waveOptions.line;
-
-        if (line)
-        {
-            line.clear();
+        options: {
+            width: 2,
+            color: 0xffffff
+        },
+        wave: {
+            amplitude: 0,
+            period: 1,
+            phase: 0
         }
-        else
-        {
-            line = waveOptions.line = new PIXI.Graphics();
-        }
-
-        line.lineStyle(1, options.color, 1);
-
-        for(var angle = 0; angle < pixiApp.renderer.width-1; angle++)
-        {
-            var c = 360/pixiApp.renderer.width * wave.period;
-
-            var y1 = Math.sin((toRad(angle) - (wave.phase/c)) * c);
-            var y2 = Math.sin((toRad(angle+1) - (wave.phase/c)) * c);
-            var shapedY1 = pixiApp.renderer.height/2 - (wave.amplitude * y1);
-            var shapedY2 = pixiApp.renderer.height/2 - (wave.amplitude * y2);
-
-            line.moveTo(angle, shapedY1);
-            line.lineTo(angle+1, shapedY2);
-        }
-        pixiApp.stage.addChild(line);
     }
-}
+];
 
-function resize(renderer)
-{
-    renderer.resize(document.body.clientWidth, document.body.clientWidth * 0.5625);
-}
-
-// default wave is additive of all user-created waves
-window.waves = [{
-    options:{color:0xffffff},
-    wave:{
-        amplitude:0,
-        period:1,
-        phase:0
-    }
-}];
-
-var display = document.getElementById('display');
-var pixiApp = new PIXI.Application({
+let display = document.getElementById("display");
+let pixiApp = new PIXI.Application({
     width: document.body.clientWidth,
-    height: document. body.clientWidth * 0.5625,
+    height: getWaveDisplayHeight(),
     antialias: true,
     transparent: false,
     resolution: 1,
 
-    backgroundColor:0x0
+    backgroundColor: 0x0
 });
 
-document.addEventListener('waveChange', function(e)
-{
-    //var wave = e.detail.wave;
-    //window.waves[0].wave.amplitude += wave.amplitude;
-    //window.waves[0].wave.period += wave.period;
-    //window.waves[0].wave.phase += wave.phase;
-});
+document.getElementById("createwave").addEventListener("click", createWave);
 
-document.getElementById('createwave').addEventListener('click', createWave);
-
-window.addEventListener('resize', resize.bind(null, pixiApp.renderer));
+window.addEventListener("resize", resize.bind(null, pixiApp.renderer));
 
 display.appendChild(pixiApp.view);
 
-pixiApp.ticker.add(delta => draw(delta, pixiApp));
+pixiApp.ticker.add((delta) => draw(delta, pixiApp));
